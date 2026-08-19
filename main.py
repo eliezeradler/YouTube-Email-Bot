69cdcbe3be8c95a41cc69241248d419697c0ae31
eliezeradler <aa0527649130@gmail.com>
2026-08-19 13:21:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ def process_email(drive_svc, gmail_svc, msg_id):
                 else:
                     # 🔥 שדרוג איכות הוידאו ל-1080p
                     ydl_opts.update({
-                        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+                        'format': 'bestvideo[ext=mp4]+bestaudio/best[ext=mp4]/best',
                         'merge_output_format': 'mp4'
                     })
 
